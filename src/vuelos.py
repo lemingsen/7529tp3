58ceed4d73ca32fb2d3b2fee4b3a7af452b3a4ad
desencuentro <->
2021-11-08 08:55:29 -0300

    +test_sin_argumentos => imprimirAyuda

--- src/vuelos.py
+++ src/vuelos.py
@@ -1,2 +1,22 @@
+import sys
+
 class Vuelos:
-    pass
+    def __init__(self,args):
+        if 1 != len(args):
+            self.imprimirAyuda()
+
+    def imprimirAyuda(self):
+        margen = "            "
+        print("Por favor, ingrese el nombre del archivo a procesar como primer parámetro del script.")
+        print("Por ejemplo:")
+        print(margen+"python -m src.vuelos tests/entradas/test_diapo3.csv\n\n")
+        print("El archivo debe contener:")
+        print(" * El nombre de la ruta de origen en la primera línea.")
+        print(" * El nombre de la ruta de destino en la segimda línea.")
+        print(" * A partir de la tercera línea, una ruta por línea: con origen, destino y costo separados por coma.")
+        print("Por ejemplo:\n" + margen+"NQN\n" + margen+"ROS")
+        print(margen+"AEP, MDQ, 30\n"+margen+"AEP, COR, 60\n" + margen+"IGR,AEP,120")
+        print(margen+"COR,ROS, 30\n" + margen+"MDZ, AEP,70\n" + margen+"NQN,IGR,150")
+
+if "__main__" == __name__:
+    Vuelos(sys.argv[1:])
--- tests/test_vuelos.py
+++ tests/test_vuelos.py
@@ -1,10 +1,57 @@
+import io
+import re
 import os
+import subprocess
+import sys
 import unittest
+from math import inf
 
 from src.vuelos import Vuelos
 
 class TestVuelos(unittest.TestCase):
-    pass
+        ################################ AUXILIARES ################################
+    def pathArchivo(self, rutaRelativa):
+        return os.path.realpath(os.path.join(os.path.dirname(__file__), rutaRelativa))
+
+    def ejecutar(self,argumentos,guardar=True,charset='windows-1252'):
+        args = [sys.executable, "-m", "src.vuelos",*argumentos]
+        cwd = self.pathArchivo("../")
+        self._primera = None
+        self._ultima = None
+
+        with subprocess.Popen(args,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=None,shell=False,cwd=cwd) as proc:
+            primera = proc.stdout.readline().decode(charset)
+            if (1 == guardar) or (True is guardar):
+                self._primera = primera
+            yield primera
+
+            ultima = primera
+            while ultima:
+                ultima = proc.stdout.readline().decode(charset)
+                if not ultima:
+                    return
+                if (-1 == guardar) or (True is guardar):
+                    self._ultima = ultima
+                yield ultima
+
+    def lineaCumple(self, lineas, fnTextoEnLinea, verbose=True):
+        cumple = any(fnTextoEnLinea(linea) for linea in lineas)
+        if verbose and not cumple:
+            if self._primera:
+                print("Primera: ", self._primera)
+            if self._ultima:
+                print("Última: ", self._primera)
+        return cumple
+
+    def setUp(self):
+        self._python = "python"
+
+
+    ################################ TESTS ################################
+    def test_sin_argumentos(self):
+        out = self.ejecutar([])
+
+        self.assertTrue(self.lineaCumple(out, lambda txt: ("nombre" in txt) and ("archivo" in txt)))
 
 if __name__ == '__main__':
     unittest.main()
