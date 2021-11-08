f3136421c83bb2b66b23c81b7cc3bdbbf45a0644
desencuentro <->
2021-11-08 01:49:11 -0300

    test_valor_recien_creado

--- src/arcodirecto.py
+++ src/arcodirecto.py
@@ -1,6 +1,10 @@
 class ArcoDirecto:
-    def __init__(self,capacidad):
+    def __init__(self,capacidad,flujo=0):
         self._capacidad = capacidad
+        self._flujo = flujo
 
     def capacidad(self):
         return self._capacidad
+
+    def valor(self):
+        return self._flujo
--- tests/test_arcodirecto.py
+++ tests/test_arcodirecto.py
@@ -10,5 +10,12 @@ class TestArcoDirecto(unittest.TestCase):
         self.assertEqual(arco1.capacidad(),3)
         self.assertEqual(arco2.capacidad(),34)
 
+    def test_valor_recien_creado(self):
+        arco1 = ArcoDirecto(7)
+        arco2 = ArcoDirecto(8,flujo=2)
+
+        self.assertEqual(arco1.valor(),0)
+        self.assertEqual(arco2.valor(),2)
+
 if __name__ == '__main__':
     unittest.main()
