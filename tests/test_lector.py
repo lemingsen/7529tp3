435a47688f302b0a6edcd956a15d50eda634ab3c
desencuentro <->
2021-11-08 08:27:41 -0300

    +test_diapo3

--- tests/test_lector.py
+++ tests/test_lector.py
@@ -75,5 +75,26 @@ class TestLector(unittest.TestCase):
         rutaArchivo = self.pathArchivo("entradas/test_origen_inexistente.txt")
         self.assertRaises(Exception,Lector,rutaArchivo)
 
+    def test_diapo3(self):
+        """De la diapositva nº 3 «Ford Fulkerson, Paso a paso»"""
+        rutaArchivo = self.pathArchivo("entradas/test_diapo3.txt")
+        lector = Lector(rutaArchivo)
+        grafo = lector.grafo
+
+        self.assertEqual(lector.desde, "s")
+        self.assertEqual(lector.hasta, "t")
+        self.assertEqual(9,grafo.cantidadNodos())
+        self.assertEqual(12,grafo.cantidadArcos())
+        self.assertEqual(grafo.idDeNodoAlias("a"),0)
+        self.assertEqual(grafo.idDeNodoAlias("b"),1)
+        self.assertEqual(grafo.idDeNodoAlias("c"),2)
+        self.assertEqual(grafo.idDeNodoAlias("d"),3)
+        self.assertEqual(grafo.idDeNodoAlias("e"),4)
+        self.assertEqual(grafo.idDeNodoAlias("f"),5)
+        self.assertEqual(grafo.idDeNodoAlias("g"),6)
+        self.assertEqual(list(grafo.arcos()),[
+            (0,1, 5), (0,3,10), (1,8,5), (2,1,5), (2,5,8), (3,2, 2),
+            (3,4,10), (4,8,10), (5,6,9), (6,8,8), (7,0,5), (7,2,20)])
+
 if __name__ == '__main__':
     unittest.main()
